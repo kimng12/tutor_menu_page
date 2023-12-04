33ed33b4193901d167342ee5f5a698ba9621bd77
trungthanhly <119275248+jaesungl33@users.noreply.github.com>
2023-12-04 16:50:53 +0700

    Lan nay thi anh chiu thuaaaa em roi
    ryderrrrrrrrrrrrrrrr

--- test/test.js
+++ test/test.js
@@ -1,5 +1,9 @@
-document.getElementById('tutors').addEventListener('click', function(event) {
-    if (event.target.tagName === 'BUTTON') {
-        alert('Viewing Tutor Profile');
-    }
-});
+// Add any JavaScript you need for interaction
+document.getElementById('book-trial').addEventListener('click', function() {
+    alert('Booking trial lesson...');
+  });
+  
+  document.getElementById('more-info').addEventListener('click', function() {
+    alert('Showing more information...');
+  });
+  
